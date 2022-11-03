e920305cbf670f810f5cde149d04d65772e30c36
dmitry-weirdo <dmitry.weirdo@gmail.com>
2022-11-03 20:02:33 +0100

    feat: Add a draft of Team model class.

--- src/main/kotlin/com/chgk/Main.kt
+++ src/main/kotlin/com/chgk/Main.kt
@@ -1,5 +1,6 @@
 package com.chgk
 
+import com.chgk.model.Team
 import com.chgk.model.Tournament
 import org.apache.logging.log4j.kotlin.Logging
 
@@ -22,6 +23,22 @@ class Main : Logging {
                 Tours count: ${tournament.toursCount}
                 Questions per tour: ${tournament.questionsPerTour}
                 Total questions in the tournament: ${tournament.totalQuestions}
+            """.trimIndent())
+
+            val team = Team(
+                67959,
+                "Севрюга",
+                null
+            )
+
+            logger.info("""
+                Team id: ${team.id}
+                Team name: ${team.name}
+                Team city: ${team.city}
+                Team has city: ${team.hasCity}
+                Team is national (сборная): ${team.isNational}
+                Link in old rating: ${team.linkInOldRating}
+                Link in new rating: ${team.linkInNewRating}
             """.trimIndent())
         }
     }
---
+++ src/main/kotlin/com/chgk/model/Team.kt
@@ -0,0 +1,27 @@
+package com.chgk.model
+
+/**
+ * Команда по ЧГК.
+ * @property id идентификатор команды на сайтах рейтинга
+ * @property name название команды
+ * @property city город, из которого команда. Если команда сборная, то <code>null</code>.
+ */
+data class Team (
+    val id: Int,
+    val name: String,
+    val city: String?, // todo: probably make city a separate class /  enum
+    // todo: отличать сборные
+    // todo: состав команды
+) {
+    val linkInOldRating: String
+        get() = "https://rating.chgk.info/team/${id}"
+
+    val linkInNewRating: String
+        get() = "https://rating.maii.li/b/team/${id}/"
+
+    val hasCity: Boolean
+        get() = (city != null)
+
+    val isNational: Boolean // сборная // todo: better naming if possible
+        get() = !hasCity
+}
